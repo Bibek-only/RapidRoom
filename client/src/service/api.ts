e350991ca3db15211c073262f77165ca0944834c
Bibek-only <bibekbibek966@gmail.com>
2026-07-21 02:11:32 +0530

    deploy 3

--- client/src/service/api.ts
+++ client/src/service/api.ts
@@ -1,4 +1,4 @@
 // const API = "https://rapid-room-server.onrender.com/api/v1"; // Deployed API
-const API = "https://rapidroom.netlify.app/api/v1";
+const API = "https://rapidroom.onrender.com/api/v1";
 export default API;
 
